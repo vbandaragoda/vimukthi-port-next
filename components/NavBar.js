32f30a128383f28f820ac2a975a04a7127445529
Vimukthi Bandaragoda <vimukthi.nelanga@gmail.com>
2023-10-06 17:43:11 +0530

    hero changes

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,76 +1,81 @@
 import React, { useState } from "react";
+import "./navbar.css";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 
-const NavBar = () => {
-  const [isNavOpen, setIsNavOpen] = useState(false);
+function Navbar({ darkMode, toggleDarkMode }) {
+  // const [darkMode, setDarkMode] = useState(true);
+  const [active, setActive] = useState("nav__menu");
+  const [icon, setIcon] = useState("nav__toggler");
+  
+  const navToggle = () => {
+    if (active === "nav__menu") {
+      setActive("nav__menu nav__active");
+    } else setActive("nav__menu");
 
-  const toggleNav = () => {
-    setIsNavOpen(!isNavOpen);
+    // Icon Toggler
+    if (icon === "nav__toggler") {
+      setIcon("nav__toggler toggle");
+    } else setIcon("nav__toggler");
   };
-
   return (
-    <nav className="py-10 mb-12 flex justify-between">
+    <nav className={`py-10 mb-1 flex justify-between ${darkMode ? 'dark' : ''}`}>
       <h1 className="text-xl font-burtons dark:text-white">
         <a href="/">vimukthi</a>
       </h1>
 
-      <button
-        onClick={toggleNav}
-        type="button"
-        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-        aria-expanded={isNavOpen}
-      >
-        <span className="sr-only">Open main menu</span>
-        <svg
-          className="w-5 h-5"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 17 14"
-        >
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M1 1h15M1 7h15M1 13h15"
+      <ul className={active}>
+        <li className="nav__item">
+          <a href="/" className="nav__link">
+            Home
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="/about" className="nav__link">
+            About
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Portfolio
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Services
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Contact
+          </a>
+        </li>
+        
+      </ul>
+      <ul className="flex items-center">
+        <li>
+          <BsFillMoonStarsFill
+            onClick={toggleDarkMode}
+            className={`cursor-pointer text-2xl ${darkMode ? 'dark:text-white' : 'text-black'}`}
           />
-        </svg>
-      </button>
-
-      <div
-        className={`${
-          isNavOpen ? "block md:hidden" : "hidden md:block"
-        } w-full md:w-auto`}
-        id="navbar-default"
-      >
-        <ul className="flex flex-col items-start mb-6 text-sm font-medium dark:text-white md:flex-row md:mb-0">
-          <li className="mb-3 md:mb-0 hover:underline md:mr-6">About</li>
-          <li className="mb-3 md:mb-0 hover:underline md:mr-6">Services</li>
-          <li className="mb-3 md:mb-0 hover:underline md:mr-6">Portfolio</li>
-          <li className="hover:underline">Contact</li>
-        </ul>
-        <ul className="flex items-center">
-          <li>
-            <BsFillMoonStarsFill
-              onClick={() => setDarkMode(!darkMode)} 
-              className="cursor-pointer text-2xl dark:text-white"
-            />
-          </li>
-          <li>
-            <a
-              href="https://drive.google.com/file/d/1mYu3gi8OIWL9xb2_UMhZNHTu1b-0hjyh/view?usp=drive_link"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-white bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
-            >
-              Resume
-            </a>
-          </li>
-        </ul>
+        </li>
+        <li>
+          <a
+            href="https://drive.google.com/file/d/1mYu3gi8OIWL9xb2_UMhZNHTu1b-0hjyh/view?usp=drive_link"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+          >
+            Resume
+          </a>
+        </li>
+      </ul>
+      <div onClick={navToggle} className={icon}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
       </div>
     </nav>
   );
-};
+}
 
-export default NavBar;
+export default Navbar;
---
+++ components/xNavBar.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+
+const NavBar = () => {
+  const [darkMode, setDarkMode] = useState(true);
+  
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
+  return (
+    <nav className="py-10 mb-12 flex justify-between">
+      <h1 className="text-xl font-burtons dark:text-white">
+        <a href="/">vimukthi</a>
+      </h1>
+
+      <button
+        onClick={toggleNav}
+        type="button"
+        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        aria-expanded={isNavOpen}
+      >
+        <span className="sr-only">Open main menu</span>
+        <svg
+          className="w-5 h-5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 17 14"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M1 1h15M1 7h15M1 13h15"
+          />
+        </svg>
+      </button>
+
+      <div
+        className={`${
+          isNavOpen ? "block md:hidden" : "hidden md:block"
+        } w-full md:w-auto`}
+        id="navbar-default"
+      >
+        <ul className="flex flex-col items-start mb-6 text-sm font-medium dark:text-white md:flex-row md:mb-0">
+          <li className="mb-3 md:mb-0 hover:underline md:mr-6">About</li>
+          <li className="mb-3 md:mb-0 hover:underline md:mr-6">Services</li>
+          <li className="mb-3 md:mb-0 hover:underline md:mr-6">Portfolio</li>
+          <li className="hover:underline">Contact</li>
+        </ul>
+        <ul className="flex items-center">
+          <li>
+            <BsFillMoonStarsFill
+              onClick={() => setDarkMode(!darkMode)} 
+              className="cursor-pointer text-2xl dark:text-white"
+            />
+          </li>
+          <li>
+            <a
+              href="https://drive.google.com/file/d/1mYu3gi8OIWL9xb2_UMhZNHTu1b-0hjyh/view?usp=drive_link"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+            >
+              Resume
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
