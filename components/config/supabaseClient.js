3de903134b54dd3ebe127b012f34ddb57c67edf8
Vimukthi Bandaragoda <vimukthi.nelanga@gmail.com>
2023-11-23 01:02:45 +0530

    supabase removed

--- components/config/supabaseClient.js
+++ components/config/supabaseClient.js
@@ -1,8 +1,8 @@
-import { createClient } from "@supabase/supabase-js";
+// import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_REACT_APP_ANON_KEY;
+// const supabaseUrl = process.env.NEXT_PUBLIC_REACT_APP_SUPABASE_URL;
+// const supabaseAnonKey = process.env.NEXT_PUBLIC_REACT_APP_ANON_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-export default supabase;
+// export default supabase;
